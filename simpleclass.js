020f978b96b38a9ec269935e12f98e546247c09a
Ilya Osovets <iosovets@gmail.com>
2017-05-16 14:17:30 -0700

    added more methods

--- simpleclass.js
+++ simpleclass.js
@@ -1,14 +1,36 @@
-"use strict"
+
 console.log("simpleclass.js loaded")
-class SimpleClass {
+
+import { MyBase } from "MyBase";
+//pulls the module from tyhe mybase.js
+import { compute, val2 } from "someOtherModule"
+// plus the modules compute and val from someothermodule
+
+
+export class SimpleClass extends MyBase {
+    // simpleclass extends mybase and inherits properties from mybase
     constructor() {
+        super(); //explicitly invokes the base class instructor
         this.name = "Barney";
 
     }
+
     get message() {
-        return "Hellow es2015"
+        let threshold = 40;
+        let result = `Hello ${this.name}, are you ${5 + threshold} years old?`;
+        return result;
     }
+
     calculate() {
-        return 42;
+        return compute() + val2;
+    }
+
+    startEngine() {
+        var promise = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve("roar");
+            }, 5000);
+        });
+        return promise;
     }
 }
