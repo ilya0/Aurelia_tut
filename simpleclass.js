235d4cebaf2e1e9abb55c1fbc698e8a92f7b1e18
Ilya Osovets <iosovets@gmail.com>
2017-05-16 11:48:34 -0700

    working test

---
+++ simpleclass.js
@@ -0,0 +1,14 @@
+"use strict"
+console.log("simpleclass.js loaded")
+class SimpleClass {
+    constructor() {
+        this.name = "Barney";
+
+    }
+    get message() {
+        return "Hellow es2015"
+    }
+    calculate() {
+        return 42;
+    }
+}
